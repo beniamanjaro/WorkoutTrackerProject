3c734a3a4b41f26b31cf2e5bfbee6bb94f59a16b
Benjamin Vrajitor <benjamin.vrajitor@amdaris.com>
2022-08-31 13:54:51 +0300

    added muscles dtos

---
+++ WokroutTracker.Presentation/DTOs/PrimaryMuscleGetDto.cs
@@ -0,0 +1,8 @@
+﻿namespace WorkoutTracker.Presentation.DTOs
+{
+    public class PrimaryMuscleGetDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ WokroutTracker.Presentation/DTOs/SecondaryMuscleGetDto.cs
@@ -0,0 +1,8 @@
+﻿namespace WorkoutTracker.Presentation.DTOs
+{
+    public class SecondaryMuscleGetDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
