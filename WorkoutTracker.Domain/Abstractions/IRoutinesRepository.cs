7fb60753efac95c3263be69a180c5704935f7d6d
Benjamin Vrajitor <benjamin.vrajitor@amdaris.com>
2022-08-31 13:34:06 +0300

    updated routines rep interface

--- WorkoutTracker.Domain/Abstractions/IRoutinesRepository.cs
+++ WorkoutTracker.Domain/Abstractions/IRoutinesRepository.cs
@@ -4,9 +4,9 @@ namespace WorkoutTracker.Domain.Abstractions
 {
     public interface IRoutinesRepository
     {
-        Task<Routine> CreateRoutine(Routine routine);
-        void DeleteRoutineById(int id);
-        Task<List<Routine>> GetAllRoutines(int id);
+        Task<Routine> AddRoutine(Routine routine);
+        void DeleteRoutine(Routine routine);
+        Task<List<Routine>> GetAllRoutines();
         Task<Routine> GetRoutineById(int id);
         void UpdateRoutineById(int id, Routine routine);
     }
