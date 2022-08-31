973151d33154e3fd3ab435a49d4af3bdfa18b213
Benjamin Vrajitor <benjamin.vrajitor@amdaris.com>
2022-08-31 13:45:44 +0300

    updated user model

--- WorkoutTracker.Domain/Models/User.cs
+++ WorkoutTracker.Domain/Models/User.cs
@@ -15,53 +15,7 @@ namespace WorkoutTracker.Domain.Models
         public string Password { get; set; }
         public int? SelectedWorkoutPlanId { get; set; }
         public ICollection<WorkoutPlan> WorkoutPlans { get; set; }
-
-        public void ExportWorkout(int workoutId, string filePath)
-        {
-            try
-            {
-                var workoutPlan = WorkoutPlans.First(w => w.Id == workoutId);
-                var routines = workoutPlan.Routines;
-                using (StreamWriter file = new(@filePath, false))
-                {
-                    file.WriteLine(workoutPlan.Name);
-                    file.WriteLine();
-                    foreach (Routine routine in routines)
-                    {
-                        file.WriteLine(routine.Name);
-                        foreach (WorkoutSet workoutSet in routine.WorkoutSets)
-                        {
-                            foreach (Set set in workoutSet.Sets)
-                            {
-                                file.WriteLine($"{set.Exercise.Name}, {set.NumberOfReps}, {set.Weight}");
-                            }
-                            file.WriteLine();
-                        }
-                    }
-                }
-
-
-            }
-            catch (Exception ex)
-            {
-                throw new ApplicationException("exception", ex);
-
-            }
-        }
-
-        public void AddWorkoutPlan(WorkoutPlan workoutPlan)
-        {
-            WorkoutPlans.Add(workoutPlan);
-        }
-
-
-        //public void CompleteRoutine()
-        //{
-        //    CompletedRoutines.Add(SelectedWorkoutPlan.Routines[0]);
-        //    var temp = SelectedWorkoutPlan.Routines[0];
-        //    SelectedWorkoutPlan.Routines.RemoveAt(0);
-
-        //}
-
+        public ICollection<CompletedRoutine> CompletedRoutines { get; set; }
     }
 }
+
