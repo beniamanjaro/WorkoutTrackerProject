db7e3852c435031f88c8db7990a36e7b5eb7b599
Benjamin Vrajitor <benjamin.vrajitor@amdaris.com>
2022-08-31 13:49:28 +0300

    implemented completedRoutines repository interface

---
+++ WorkoutTracker.Domain/Abstractions/ICompletedRoutinesRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkoutTracker.Domain.Models;
+
+namespace WorkoutTracker.Domain.Abstractions
+{
+    public interface ICompletedRoutinesRepository
+    {
+        Task<CompletedRoutine> AddCompletedRoutine(CompletedRoutine completedRoutine);
+        Task<CompletedRoutine> GetCompletedRoutineById(int id);
+        Task<List<CompletedRoutine>> GetWorkoutPlansByUser(int userId);
+        Task UpdateCompletedRoutine(CompletedRoutine completedRoutine);
+        void DeleteCompletedRoutine(CompletedRoutine completedRoutine);
+
+    }
+}
