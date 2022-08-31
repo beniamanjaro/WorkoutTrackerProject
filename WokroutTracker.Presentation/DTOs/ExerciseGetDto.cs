905fb95f82c8e17e442cff2b86b64eec1129bbef
Benjamin Vrajitor <benjamin.vrajitor@amdaris.com>
2022-08-31 13:43:16 +0300

    added exercise dtos

---
+++ WokroutTracker.Presentation/DTOs/ExerciseGetDto.cs
@@ -0,0 +1,14 @@
+﻿using WorkoutTracker.Domain.Models;
+
+namespace WorkoutTracker.Presentation.DTOs
+{
+    public class ExerciseGetDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Equipment { get; set; }
+        public ICollection<PrimaryMuscleGetDto> PrimaryMuscles {get; set;}
+        public ICollection<SecondaryMuscleGetDto> SecondaryMuscles {get; set;}
+    }
+}
---
+++ WokroutTracker.Presentation/DTOs/ExercisePostDto.cs
@@ -0,0 +1,9 @@
+﻿namespace WorkoutTracker.Presentation.DTOs
+{
+    public class ExercisePostDto
+    {
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Equipment { get; set; }
+    }
+}
