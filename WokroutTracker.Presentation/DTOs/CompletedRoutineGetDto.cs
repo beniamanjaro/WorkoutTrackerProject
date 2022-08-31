9708a77f33475189d6600a8f8243f8b3cf181ba3
Benjamin Vrajitor <benjamin.vrajitor@amdaris.com>
2022-08-31 13:54:42 +0300

    added completedRoutine dtos

---
+++ WokroutTracker.Presentation/DTOs/CompletedRoutineGetDto.cs
@@ -0,0 +1,12 @@
+﻿using WorkoutTracker.Domain.Models;
+
+namespace WorkoutTracker.Presentation.DTOs
+{
+    public class CompletedRoutineGetDto
+    {
+        public int CompletedRoutineId { get; set; }
+        public string Name { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public ICollection<WorkoutSetGetDto> WorkoutSets { get; set; }
+    }
+}
---
+++ WokroutTracker.Presentation/DTOs/CompletedRoutinePutPostDto.cs
@@ -0,0 +1,10 @@
+﻿using WorkoutTracker.Domain.Models;
+
+namespace WorkoutTracker.Presentation.DTOs
+{
+    public class CompletedRoutinePutPostDto
+    {
+        public string Name { get; set; }
+        public ICollection<WorkoutSetPostDto> WorkoutSets { get; set; }
+    }
+}
