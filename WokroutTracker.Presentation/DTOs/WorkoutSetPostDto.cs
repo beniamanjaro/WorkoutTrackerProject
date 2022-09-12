fe2a868c9bac336daf743c1056ac63e9d301bdbb
Benjamin Vrajitor <benjamin.vrajitor@amdaris.com>
2022-09-12 23:24:40 +0300

    updated workout set dtos

--- WokroutTracker.Presentation/DTOs/WorkoutSetPostDto.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace WorkoutTracker.Presentation.DTOs
-{
-    public class WorkoutSetPostDto
-    {
-        public ICollection<SetPostDto> Sets { get; set; }
-    }
-}
---
+++ WokroutTracker.Presentation/DTOs/WorkoutSetPutPostDto.cs
@@ -0,0 +1,7 @@
+﻿namespace WorkoutTracker.Presentation.DTOs
+{
+    public class WorkoutSetPutPostDto
+    {
+        public ICollection<SetPutPostDto> Sets { get; set; }
+    }
+}
