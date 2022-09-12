5633970fe4f18688bd2618cb1c7307b933013e77
Benjamin Vrajitor <benjamin.vrajitor@amdaris.com>
2022-09-12 23:30:53 +0300

    populated db context with some exercises

--- WorkoutTracker.Infrastructure/WorkoutContext.cs
+++ WorkoutTracker.Infrastructure/WorkoutContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,15 +9,12 @@ using WorkoutTracker.Domain.Models;
 
 namespace WorkoutTracker.Infrastructure
 {
-    public class WorkoutContext : DbContext
+    public class WorkoutContext : IdentityDbContext
     {
 
-        public WorkoutContext()
-        {
+        public WorkoutContext() : base() { }
 
-        }
-
-        public WorkoutContext (DbContextOptions<WorkoutContext> optionsBuilder) : base(optionsBuilder) 
+    public WorkoutContext (DbContextOptions<WorkoutContext> optionsBuilder) : base(optionsBuilder) 
         {
 
         }
@@ -39,53 +37,53 @@ namespace WorkoutTracker.Infrastructure
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
             modelBuilder.Entity<Exercise>().HasData(
+                
                 new Exercise() { Id = 1, Name = "Bench Press", Category = "Chest", Equipment = "Bench, Barbell" },
                 new Exercise() { Id = 2, Name = "Cable Crossover", Category = "Chest", Equipment = "Cable Machine" },
                 new Exercise() { Id = 3, Name = "Dumbbell Flies", Category = "Chest", Equipment = "Bench, Dumbbells" },
                 new Exercise() { Id = 4, Name = "Dumbbell Press", Category = "Chest", Equipment = "Bench, Dumbbells" },
-                new Exercise() { Id = 5, Name = "Incline Benchpress", Category = "Chest", Equipment = "Bench, Barbell" },
+                new Exercise() { Id = 5, Name = "Incline Bench Press", Category = "Chest", Equipment = "Bench, Barbell" },
                 new Exercise() { Id = 6, Name = "Incline Dumbbell Press", Category = "Chest", Equipment = "Bench, Dumbbells" },
-                new Exercise() { Id = 8, Name = "Squat", Category = "Legs", Equipment = "Barbell" },
-                new Exercise() { Id = 9, Name = "Calf Raises", Category = "Legs", Equipment = "" },
-                new Exercise() { Id = 10, Name = "Front Squat", Category = "Legs", Equipment = "Barbell" },
-                new Exercise() { Id = 11, Name = "Leg Curls", Category = "Legs", Equipment = "Leg Curls Machine" },
-                new Exercise() { Id = 12, Name = "Leg Extensions", Category = "Legs", Equipment = "Leg Extension Machine" },
-                new Exercise() { Id = 13, Name = "Leg Press", Category = "Legs", Equipment = "Leg Press Machine" }
+                new Exercise() { Id = 7, Name = "Squat", Category = "Legs", Equipment = "Barbell" },
+                new Exercise() { Id = 8, Name = "Calf Raises", Category = "Legs", Equipment = "" },
+                new Exercise() { Id = 9, Name = "Front Squat", Category = "Legs", Equipment = "Barbell" },
+                new Exercise() { Id = 10, Name = "Leg Curls", Category = "Legs", Equipment = "Leg Curls Machine" },
+                new Exercise() { Id = 11, Name = "Leg Extensions", Category = "Legs", Equipment = "Leg Extension Machine" },
+                new Exercise() { Id = 12, Name = "Leg Press", Category = "Legs", Equipment = "Leg Press Machine" },
+                new Exercise() { Id = 13, Name = "Lunges", Category = "Legs", Equipment = "Barbell" },
+                new Exercise() { Id = 14, Name = "Seated Calf Raises", Category = "Legs", Equipment = "" },
+                new Exercise() { Id = 15, Name = "Dumbbell Lateral Raises", Category = "Shoulders", Equipment = "Dumbbell" },
+                new Exercise() { Id = 16, Name = "Military Press", Category = "Shoulders", Equipment = "Barbell" },
+                new Exercise() { Id = 17, Name = "Shoulder Dumbbell Press", Category = "Shoulders", Equipment = "Dumbbell" },
+                new Exercise() { Id = 18, Name = "Upright Rows", Category = "Shoulders", Equipment = "Barbell" },
+                new Exercise() { Id = 19, Name = "Assited Dips", Category = "Triceps", Equipment = "" },
+                new Exercise() { Id = 20, Name = "Close Grip Bench Press", Category = "Triceps", Equipment = "Barbell Bench" },
+                new Exercise() { Id = 21, Name = "Dips", Category = "Triceps", Equipment = "" },
+                new Exercise() { Id = 22, Name = "Pushdowns", Category = "Triceps", Equipment = "Cable" },
+                new Exercise() { Id = 23, Name = "Assisted Chin Up", Category = "Back", Equipment = "" },
+                new Exercise() { Id = 24, Name = "Assisted Pull Up", Category = "Back", Equipment = "" },
+                new Exercise() { Id = 25, Name = "Pull up", Category = "Back", Equipment = "" },
+                new Exercise() { Id = 26, Name = "Chin Up", Category = "Back", Equipment = "" },
+                new Exercise() { Id = 27, Name = "Barbell Row", Category = "Back", Equipment = "Barbell" },
+                new Exercise() { Id = 28, Name = "Cable Row", Category = "Back", Equipment = "Cable" },
+                new Exercise() { Id = 29, Name = "Deadlift", Category = "Back", Equipment = "Barbell" },
+                new Exercise() { Id = 30, Name = "Dumbbell Row", Category = "Back", Equipment = "Dumbbell" },
+                new Exercise() { Id = 31, Name = "Hyperextensions", Category = "Back", Equipment = "" },
+                new Exercise() { Id = 32, Name = "Pulldowns", Category = "Back", Equipment = "Cable" }
+                
                 );
+            modelBuilder.Entity<CompletedRoutine>()
+            .HasOne<User>()
+            .WithMany(u => u.CompletedRoutines)
+            .HasForeignKey(u => u.UserId)
+            .OnDelete(DeleteBehavior.Restrict);
 
-            modelBuilder.Entity<PrimaryMuscle>().HasData(
-                new PrimaryMuscle() { Id = 1, ExerciseId = 1, Name = "Chest" }
-                );
-
-            modelBuilder.Entity<User>().HasData(
-                new User () { Id = 1, Email = "email@gmail.com", Password = "test123", Username = "test user" }
-                  );
-
-            modelBuilder.Entity<WorkoutPlan>().HasData(
-                new WorkoutPlan() { Id = 1, Name = "Push Pull Legs", TimesPerWeek = 6, UserId = 1  }
-                 );
-
-            modelBuilder.Entity<Routine>().HasData(
-                new Routine() { Id = 1, WorkoutPlanId = 1, DayOrderNumber = 1, Name = "Chest day" }
-                 );
-
-            modelBuilder.Entity<WorkoutSet>().HasData(
-                new WorkoutSet() { Id = 1, RoutineId = 1 },
-                new WorkoutSet() { Id = 2, RoutineId = 1 }
-
-                 );
-
-            modelBuilder.Entity<Set>().HasData(
-                new Set() { Id = 1, ExerciseId = 1, WorkoutSetId = 1, NumberOfReps = 8, Weight = 80 },
-                new Set() { Id = 2, ExerciseId = 1, WorkoutSetId = 1, NumberOfReps = 8, Weight = 70 },
-                new Set() { Id = 3, ExerciseId = 1, WorkoutSetId = 1, NumberOfReps = 8, Weight = 60 },
-                new Set() { Id = 4, ExerciseId = 2, WorkoutSetId = 2, NumberOfReps = 5, Weight = 55 },
-                new Set() { Id = 5, ExerciseId = 2, WorkoutSetId = 2, NumberOfReps = 5, Weight = 45 }
-                 );
-
-
-
+            modelBuilder.Entity<User>()
+            .HasMany(u => u.CompletedRoutines)
+            .WithOne(cr => cr.User)
+            .OnDelete(DeleteBehavior.Restrict);
 
 
 
