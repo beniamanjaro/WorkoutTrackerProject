d4889fb5f3074eb606440f6658c88a2d1fe666f7
Benjamin Vrajitor <benjamin.vrajitor@amdaris.com>
2022-09-12 23:23:11 +0300

    updated workoutplan dtos

--- WokroutTracker.Presentation/DTOs/WorkoutPlanPostDto.cs
+++ WokroutTracker.Presentation/DTOs/WorkoutPlanPostDto.cs
@@ -2,11 +2,11 @@
 
 namespace WorkoutTracker.Presentation.DTOs
 {
-    public class WorkoutPlanPutPostDto
+    public class WorkoutPlanPostDto
     {
         public int UserId { get; set; }
         public string Name { get; set; }
         public int TimesPerWeek { get; set; }
-        public ICollection<RoutinePostDto> Routines { get; set; }
+        public ICollection<RoutinePutPostDto> Routines { get; set; }
     }
 }
---
+++ WokroutTracker.Presentation/DTOs/WorkoutPlanPutDto.cs
@@ -0,0 +1,12 @@
+﻿using WorkoutTracker.Domain.Models;
+
+namespace WorkoutTracker.Presentation.DTOs
+{
+    public class WorkoutPlanPutDto
+    {
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public int TimesPerWeek { get; set; }
+        public ICollection<Routine> Routines { get; set; }
+    }
+}
